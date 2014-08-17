024e693da1122e705a0e2e03c66d7975ba5d4d0b
mattdesl <dave.des@gmail.com>
2014-08-17 16:51:46 -0400

    adding source

---
+++ index.js
@@ -0,0 +1,151 @@
+var createShader = require('gl-shader-core')
+
+var POSITION_ATTRIBUTE = 'position',
+    NORMAL_ATTRIBUTE = 'normal',
+    COLOR_ATTRIBUTE = 'color',
+    TEXCOORD_ATTRIBUTE = 'texcoord';
+
+function idt(out) { //TODO: move to stackgl/gl-mat4
+    out[0] = 1;
+    out[1] = 0;
+    out[2] = 0;
+    out[3] = 0;
+    out[4] = 0;
+    out[5] = 1;
+    out[6] = 0;
+    out[7] = 0;
+    out[8] = 0;
+    out[9] = 0;
+    out[10] = 1;
+    out[11] = 0;
+    out[12] = 0;
+    out[13] = 0;
+    out[14] = 0;
+    out[15] = 1;
+    return out
+}
+
+module.exports = function(gl, options) {
+    options = options||{}
+    options.texcoord = typeof options.texcoord === 'number' 
+                ? options.texcoord : (options.texcoord||0)
+        
+    var shaderSource = module.exports.generate(options),
+        vert = shaderSource.vertex,
+        frag = shaderSource.fragment
+    
+    var uniforms = [
+        { type: 'mat4', name: 'projection' },
+        { type: 'mat4', name: 'view' },
+        { type: 'mat4', name: 'model' },
+        { type: 'vec4', name: 'tint' }
+    ]
+    var attribs = [
+        { type: 'vec4', name: POSITION_ATTRIBUTE }
+    ]
+
+    for (var i=0; i<options.texcoord; i++) {
+        uniforms.push({ type: 'sampler2D', name: 'texture'+i })
+        attribs.push({ type: 'vec2', name: TEXCOORD_ATTRIBUTE+i })
+    }
+    if (options.normal)
+        attribs.push({ type: 'vec3', name: NORMAL_ATTRIBUTE })
+    if (options.color)
+        attribs.push({ type: 'vec4', name: COLOR_ATTRIBUTE })
+
+    var shader = createShader(gl, vert, frag, uniforms, attribs)
+    shader.bind()
+    for (var i=0; i<options.texcoord; i++) 
+        shader.uniforms['texture'+i] = i
+
+    var arr = idt( new Float32Array(16) )
+    shader.uniforms.projection = arr
+    shader.uniforms.model = arr
+    shader.uniforms.view = arr
+    shader.uniforms.tint = options.tint || [1, 1, 1, 1]
+
+    return shader
+}
+
+module.exports.generate = function(options) {
+    options = options||{}
+    options.texcoord = typeof options.texcoord === 'number' 
+                ? options.texcoord : (options.texcoord||0)
+
+    var vert = createVertexShader(options.normal, options.color, options.texcoord)
+    var frag = createFragmentShader(options.color, options.texcoord)
+    return { vertex: vert, fragment: frag }
+}
+
+function createVertexShader(hasNormals, hasColors, numTexCoords) {
+    numTexCoords = numTexCoords || 0;
+    var shader = "";
+    shader += "attribute vec4 "+POSITION_ATTRIBUTE+";\n"
+         + (hasNormals ? "attribute vec3 " + NORMAL_ATTRIBUTE + ";\n" : "")
+         + (hasColors ? "attribute vec4 " + COLOR_ATTRIBUTE + ";\n" : "");
+
+    var i;
+
+    for (i = 0; i < numTexCoords; i++) {
+        shader += "attribute vec2 " + TEXCOORD_ATTRIBUTE + i + ";\n";
+    }
+
+    shader += "uniform mat4 projection;\n";
+    shader += "uniform mat4 view;\n";
+    shader += "uniform mat4 model;\n";
+    
+    shader += (hasColors ? "varying vec4 v_col;\n" : "");
+
+    for (i = 0; i < numTexCoords; i++) {
+        shader += "varying vec2 v_tex" + i + ";\n";
+    }
+
+    shader += "void main() {\n" + "   gl_Position = projection * view * model * " + POSITION_ATTRIBUTE + ";\n"
+            + (hasColors ? "   v_col = " + COLOR_ATTRIBUTE + ";\n" : "");
+
+    for (i = 0; i < numTexCoords; i++) {
+        shader += "   v_tex" + i + " = " + TEXCOORD_ATTRIBUTE + i + ";\n";
+    }
+    shader += "   gl_PointSize = 1.0;\n";
+    shader += "}\n";
+
+    return shader;
+}
+
+function createFragmentShader(hasColors, numTexCoords) {
+    numTexCoords = numTexCoords || 0;
+    var shader = "#ifdef GL_ES\n" + "precision mediump float;\n" + "#endif\n";
+ 
+    if (hasColors) 
+        shader += "varying vec4 v_col;\n";
+
+    var i;
+    for (i = 0; i < numTexCoords; i++) {
+        shader += "varying vec2 v_tex" + i + ";\n";
+        shader += "uniform sampler2D texture" + i + ";\n";
+    }
+    shader += "uniform vec4 tint;\n",
+
+    shader += "void main() {\n" + "   gl_FragColor = ";
+
+    if (hasColors)
+        shader += "v_col"
+
+    if (hasColors && numTexCoords > 0) 
+        shader += " * ";
+    else if (!hasColors && !numTexCoords)
+        shader += "vec4(1.0)"
+
+    for (i = 0; i < numTexCoords; i++) {
+        if (i == numTexCoords - 1) {
+                shader += " texture2D(texture" + i + ",  v_tex" + i + ")";
+        } else {
+                shader += " texture2D(texture" + i + ",  v_tex" + i + ") *";
+        }
+    }
+
+    shader += " * tint"
+
+    shader += ";\n}";
+    return shader;
+}
---
+++ test.js
@@ -0,0 +1,58 @@
+var generate = require('./').generate
+var test = require('tape').test
+
+var strip = /\s*/g
+
+//this test seems fragile, to say the least
+
+var expected0 = {
+    fragment: "#ifdef GL_ES\
+        precision mediump float;\
+        #endif\
+        varying vec4 v_col;\
+        varying vec2 v_tex0;\
+        uniform sampler2D texture0;\
+        varying vec2 v_tex1;\
+        uniform sampler2D texture1;\
+        uniform vec4 tint;\
+        void main() {\
+           gl_FragColor = v_col *  texture2D(texture0,  v_tex0) * texture2D(texture1,  v_tex1) * tint;\
+        }".replace(strip, ''),
+
+    vertex: "attribute vec4 position;\
+        attribute vec4 color;\
+        attribute vec2 texcoord0;\
+        attribute vec2 texcoord1;\
+        uniform mat4 projection;\
+        uniform mat4 view;\
+        uniform mat4 model;\
+        varying vec4 v_col;\
+        varying vec2 v_tex0;\
+        varying vec2 v_tex1;\
+        void main() {\
+           gl_Position = projection * view * model * position;\
+           v_col = color;\
+           v_tex0 = texcoord0;\
+           v_tex1 = texcoord1;\
+           gl_PointSize = 1.0;\
+        }".replace(strip, '')
+}
+
+test('generates correct shader source', function(t) {
+    var source = generate({
+        normal: false,
+        texcoord: 2,
+        color: true
+    })
+    t.equal( source.vertex.replace(strip, ''), expected0.vertex, 'vertex matches' )
+    t.equal( source.fragment.replace(strip, ''), expected0.fragment, 'fragment matches' )
+
+    var source = generate({
+        texcoord: true,
+        color: true,
+        normal: false
+    })
+    console.log(source.vertex, '\n', source.fragment)
+
+    t.end()
+})
